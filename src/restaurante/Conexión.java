6a7890c9b95d55c1375261ddfd061dd63eb9b504
CT7-PC19 <CT7-PC19@DTJ-CT7-PC19>
2019-05-07 07:22:34 -0600

    si funciono

---
+++ src/restaurante/Conexión.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurante;
+
+import java.sql.*;
+import javax.swing.*;
+/**
+ *
+ * @author Javi
+ */
+public class Conexión {
+   Connection conn=null;
+    public static Connection ConnectDB(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn=DriverManager.getConnection("jdbc:mysql://localhost/bdventasph","root","");
+            return conn;
+            
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        } 
+    }
+}
